32fb1650fa830a6ae7f3025c6b0946f9d8f33607
Mark <mgarnick@techtonicgroup.com>
2019-10-22 08:28:01 -0600

    completed streetfighter selection

--- Program.cs
+++ Program.cs
@@ -16,8 +16,9 @@ namespace street_fighter_character_selection
               new string[] { "Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison" },
           };
 
-            string[] moves = new string[] { "up", "left", "right", "left", "left" };
+            // string[] moves = new string[] { "up", "left", "right", "left", "left" };
             // string[] moves = new string[] { "right", "down", "left", "left", "left", "left", "right" };
+            string[] moves = new string[] { "left", "left", "left", "left", "left", "left", "left", "left" };
             int[] position = new int[] { 0, 0 };
             StreetFighterSelection(fighters, position, moves);
         }
@@ -32,23 +33,43 @@ namespace street_fighter_character_selection
             {
                 for (int j = 0; j < fighters[i].Length; j++)
                 {
-                    if (j < moves.Length )
-                    {
-                        switch (moves[j])
-                        {
-                            case "up": startY--; if (startY < 0) { startY = 0; }; break;
-                            case "down": startY++; if (startY > fighters.Length - 1) { startY = fighters.Length - 1; }; break;
-                            case "left": startX--; if (startX < 0) { startX = fighters[startY].Length - 1; }; break;
-                            case "right": startX++; if (startX > fighters[startY].Length - 1) { startX = 0; }; break;
-                        }
-                        sff.Add(fighters[startY][startX]);
-                        WriteLine(sff[j]);
-                    };
+
                 }
 
             }
 
+            for (var i = 0; i < moves.Length; i++)
+            {
+                if (i < moves.Length)
+                {
+                    switch (moves[i])
+                    {
+                        case "up": startY--; if (startY < 0) { startY = 0; }; break;
+                        case "down": startY++; if (startY > fighters.Length - 1) { startY = fighters.Length - 1; }; break;
+                        case "left": startX--; if (startX < 0) { startX = fighters[startY].Length - 1; }; break;
+                        case "right": startX++; if (startX > fighters[startY].Length - 1) { startX = 0; }; break;
+                    }
+                    sff.Add(fighters[startY][startX]);
+                    WriteLine(sff[i]);
+                };
+            }
+
             return sff.ToArray();
         }
+
+        // public string[] StreetFighterSelection(string[][] fighters, int[] position, string[] moves)
+        // {
+        //     int x = position[1];
+        //     int y = position[0];
+        //     return moves.Select(m =>
+        //     {
+        //         x += m == "left" ? -1 : m == "right" ? 1 : 0;
+        //         y += m == "up" ? -1 : m == "down" ? 1 : 0;
+        //         x = x < 0 ? 5 : x > 5 ? 0 : x;
+        //         y = y < 0 ? 0 : y > 1 ? 1 : y;
+        //         return fighters[y][x];
+        //     }).ToArray();
+        // }
+
     }
 }
